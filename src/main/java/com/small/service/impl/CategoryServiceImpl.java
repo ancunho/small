610a49d2c7c0e804ffa5b9a49e3b09d677c5cd92
ancunho <ancunho@naver.com>
2019-10-30 17:27:10 +0800

    '20191030'

--- src/main/java/com/small/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/small/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,37 @@
 package com.small.service.impl;
 
+import com.small.common.ServerResponse;
 import com.small.service.CategoryService;
+import com.small.vo.CATEGORY;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CategoryServiceImpl implements CategoryService {
 
+    private SqlSession sqlSession;
+
+    private static final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
+    public ServerResponse addCategory(String categoryName, Integer parentId) {
+        if (parentId == null || StringUtils.isBlank(categoryName)) {
+            return ServerResponse.createByErrorMessage("添加品类参数错误");
+        }
+
+        CATEGORY category = new CATEGORY();
 
 
+        return null;
+    }
 
+    public SqlSession getSqlSession() {
+        return sqlSession;
+    }
+
+    public void setSqlSession(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
 }
